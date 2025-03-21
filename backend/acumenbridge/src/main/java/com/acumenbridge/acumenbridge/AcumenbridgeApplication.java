0f998d77aa5fcf617a2aed702236096a2f510498
Gunasekara D T C D P <chankamagunasekara@icloud.com>
2025-03-21 20:19:58 +0530

    folder Stucture
    add mongodb

---
+++ backend/acumenbridge/src/main/java/com/acumenbridge/acumenbridge/AcumenbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.acumenbridge.acumenbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcumenbridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcumenbridgeApplication.class, args);
+	}
+
+}
---
+++ backend/acumenbridge/src/test/java/com/acumenbridge/acumenbridge/AcumenbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acumenbridge.acumenbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcumenbridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
