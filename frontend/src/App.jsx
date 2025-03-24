53c6afb6285a7fbf2b8cd8d52c5212c7d79030b1
Gunasekara D T C D P <chankamagunasekara@icloud.com>
2025-03-24 15:29:16 +0530

    react upgrade to vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import axios from 'axios';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
