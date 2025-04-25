e733235523907096b2a0eff9bc5e61e5c926655f
nihara31 <niharakalansooriya11@gmail.com>
2025-04-25 23:07:13 +0530

    Implemented LearningPlan Model

---
+++ backend/acumenbridge/src/main/java/com/acumenbridge/acumenbridge/models/LearningPlan.java
@@ -0,0 +1,27 @@
+package com.acumenbridge.acumenbridge.models;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Document(collection = "learningPlans")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class LearningPlan {
+
+    @Id
+    private String id;
+
+    private String topic;
+    private String description;
+    private List<String> resources;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    
+}
