957f3267b849433e647113eaf9911c0644684821
nihara31 <niharakalansooriya11@gmail.com>
2025-04-26 08:07:02 +0530

    Created LearningPlanRepository and LearningPlanController

---
+++ backend/acumenbridge/src/main/java/com/acumenbridge/acumenbridge/controllers/LearningPlanController.java
@@ -0,0 +1,50 @@
+package com.acumenbridge.acumenbridge.controllers;
+
+import com.acumenbridge.acumenbridge.models.LearningPlan;
+import com.acumenbridge.acumenbridge.repositories.LearningPlanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/learning-plans")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+
+public class LearningPlanController {
+
+    @Autowired
+    private LearningPlanRepository repository;
+
+    @GetMapping
+    public List<LearningPlan> getAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public LearningPlan getById(@PathVariable String id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody LearningPlan plan) {
+        LearningPlan savedPlan = repository.save(plan);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body("Learning plan created successfully with ID: " + savedPlan.getId());
+    }
+
+    @PutMapping("/{id}")
+    public LearningPlan update(@PathVariable String id, @RequestBody LearningPlan updatedPlan) {
+        updatedPlan.setId(id);
+        return repository.save(updatedPlan);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        repository.deleteById(id);
+    }
+    
+}
---
+++ backend/acumenbridge/src/main/java/com/acumenbridge/acumenbridge/repositories/LearningPlanRepository.java
@@ -0,0 +1,8 @@
+package com.acumenbridge.acumenbridge.repositories;
+
+import com.acumenbridge.acumenbridge.models.LearningPlan;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LearningPlanRepository extends MongoRepository<LearningPlan, String> {
+    
+}
