f6cdda493a92017b83affd6c0405ab19d9724c7d
nihara <niharakalansooriya11@gmail.com>
2025-05-04 14:46:35 +0530

    Updated repository to find user by id

--- backend/acumenbridge/src/main/java/com/acumenbridge/acumenbridge/repositories/LearningPlanRepository.java
+++ backend/acumenbridge/src/main/java/com/acumenbridge/acumenbridge/repositories/LearningPlanRepository.java
@@ -1,10 +1,10 @@
-// repositories/LearningPlanRepository.java
-
 package com.acumenbridge.acumenbridge.repositories;
 
 import com.acumenbridge.acumenbridge.models.LearningPlan;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface LearningPlanRepository extends MongoRepository<LearningPlan, String> {
-    
+    List<LearningPlan> findByUserId(String userId);
 }
