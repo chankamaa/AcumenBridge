07cd3617a530c316059e5e379723cc6936332d1d
gayuni <gayuninuthara027@gmail.com>
2025-05-05 19:35:07 +0530

    Updated progress with user id

--- backend/acumenbridge/src/main/java/com/acumenbridge/acumenbridge/models/Progress.java
+++ backend/acumenbridge/src/main/java/com/acumenbridge/acumenbridge/models/Progress.java
@@ -1,63 +1,23 @@
+// models/Progress.java
+
 package com.acumenbridge.acumenbridge.models;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
 
+@Data
 @Document(collection = "progress")
 public class Progress {
-
     @Id
     private String id;
-
+    private String userId;
     private String achievementType;
     private String head;
     private String description;
     private int completed;
     private int toComplete;
-
-    // Getters and Setters
-    
-    public String getId() {
-        return id;
-    }
-
-    public String getAchievementType() {
-        return achievementType;
-    }
-
-    public void setAchievementType(String achievementType) {
-        this.achievementType = achievementType;
-    }
-
-    public String getHead() {
-        return head;
-    }
-
-    public void setHead(String head) {
-        this.head = head;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(int completed) {
-        this.completed = completed;
-    }
-
-    public int getToComplete() {
-        return toComplete;
-    }
-
-    public void setToComplete(int toComplete) {
-        this.toComplete = toComplete;
-    }
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
